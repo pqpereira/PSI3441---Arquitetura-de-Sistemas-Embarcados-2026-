965cc92983af50348524b9a2c32257914644df95
Lucas Pereira de Andrade <lucasp_andrade@usp.br>
2026-06-15 16:13:49 -0300

    Atividade 7 - Parte 3 utilizando semaforos

--- atividade7-shared-resources/src/main.c
+++ atividade7-shared-resources/src/main.c
@@ -3,20 +3,28 @@
 
 #define STACK_SIZE 1024
 #define PRIORITY 5
+#define CAPACIDADE_VITRINE 10
 
 int saldo_vitrine = 0;
 
 K_MUTEX_DEFINE(mutex_vitrine);
 
+K_SEM_DEFINE(sem_espacos_livres, CAPACIDADE_VITRINE, CAPACIDADE_VITRINE);
+K_SEM_DEFINE(sem_paes_disponiveis, 0, CAPACIDADE_VITRINE);
+
 void padeiro_thread(void)
 {
     while (1) {
+        k_sem_take(&sem_espacos_livres, K_FOREVER);
+
         k_mutex_lock(&mutex_vitrine, K_FOREVER);
 
         saldo_vitrine++;
         printk("Padeiro produziu 1 pao | Saldo da vitrine: %d\n", saldo_vitrine);
 
         k_mutex_unlock(&mutex_vitrine);
+
+        k_sem_give(&sem_paes_disponiveis);
 
         k_msleep(1000);
     }
@@ -25,12 +33,16 @@ void padeiro_thread(void)
 void cliente_thread(void)
 {
     while (1) {
+        k_sem_take(&sem_paes_disponiveis, K_FOREVER);
+
         k_mutex_lock(&mutex_vitrine, K_FOREVER);
 
         saldo_vitrine--;
         printk("Cliente retirou 1 pao | Saldo da vitrine: %d\n", saldo_vitrine);
 
         k_mutex_unlock(&mutex_vitrine);
+
+        k_sem_give(&sem_espacos_livres);
 
         k_msleep(1500);
     }
@@ -45,8 +57,8 @@ K_THREAD_DEFINE(cliente_id, STACK_SIZE, cliente_thread, NULL, NULL, NULL,
 int main(void)
 {
     printk("\n====================================\n");
-    printk(" Atividade 7 - Parte 2\n");
-    printk(" Utilizando Mutex\n");
+    printk(" Atividade 7 - Parte 3\n");
+    printk(" Utilizando Semaforos\n");
     printk("====================================\n\n");
 
     while (1) {
